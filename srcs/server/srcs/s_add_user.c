dfcbc67d8fb3677402e7e04e611b104dd65a8426
elenys <elenys@live.fr>
2015-09-28 09:31:40 -0700

    fix bug login syst

--- srcs/server/srcs/s_add_user.c
+++ srcs/server/srcs/s_add_user.c
@@ -7,12 +7,13 @@ int			add_user(t_auth user)
 	char	*add;
 
 	ret = ft_strjoin(user.user, ":");
-	add = ft_strjoin(ret, user.pwd);
+	add = ft_strjoin(ret, crypto(user.pwd));
 	//free(ret);
 	ret = ft_strjoin(add, "\n");
 	fd = open("./pass.txt", O_WRONLY | O_APPEND);
 	write(fd, ret, ft_strlen(ret));
 	//free(ret);
 	//free(add);
+	close(fd);
 	return (0);
 }
