29aaab871bc9ec0d496b6fe5768b9f0cb92dc72c
Risn <gthiry@student.42.fr>
2015-09-28 16:37:32 +0200

    Mon 28/09/15 16:35 - FTP - Add add_user.c & check_passwd.c

---
+++ srcs/server/srcs/add_user.c
@@ -0,0 +1,16 @@
+int			add_user(t_auth *user)
+{
+	int		fd;
+	char	*ret;
+	char	*add;
+
+	ret = ft_strjoin(user->user, ":");
+	add = ft_strjoin(ret, user->pwd);
+	free(ret);
+	ret = ft_strjoin(add, "\n");
+	fd = open("./pass.txt", O_WRONLY | O_APPEND);
+	write(fd, ret, ft_strlen(ret));
+	free(ret);
+	free(add);
+	return (0);
+}
