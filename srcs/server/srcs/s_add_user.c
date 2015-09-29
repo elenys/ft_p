237bf684edb677accb2131bb72a108b09d706c97
elenys <elenys@live.fr>
2015-09-29 10:24:37 -0700

    Deleate old password file + try to fix free in login syst

--- srcs/server/srcs/s_add_user.c
+++ srcs/server/srcs/s_add_user.c
@@ -8,7 +8,7 @@ int			add_user(t_auth user)
 
 	ret = ft_strjoin(user.user, ":");
 	add = ft_strjoin(ret, crypto(user.pwd));
-	//free(ret);
+	free(ret);
 	ret = ft_strjoin(add, "\n");
 	fd = open("./pass.txt", O_WRONLY | O_APPEND);
 	write(fd, ret, ft_strlen(ret));
