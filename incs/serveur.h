612df9c736901436a79c94460982c9308a871703
Benoit MARTINS <bmartins@e1r12p8.42.fr>
2015-09-30 15:07:22 +0200

    forget to add modfied server.h

--- incs/serveur.h
+++ incs/serveur.h
@@ -6,7 +6,7 @@
 /*   By: bmartins <bmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/03 11:05:08 by bmartins          #+#    #+#             */
-/*   Updated: 2015/09/03 15:21:03 by bmartins         ###   ########.fr       */
+/*   Updated: 2015/09/30 14:59:10 by bmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,17 @@
 #include "ftp.h"
 #include <sys/wait.h>
 
+# define CONNECTION_SUCCES 0
+# define USER_ALLREADY_EXIST -1
+# define INVALID_PASSWORD -2
+# define USER_DOES_NOT_EXIST -3
+
+# define USER_FILE "./pass.txt"
+
 # define CREATE 0
 # define CONNECTION 1
+
+
 
 typedef struct	s_auth
 {
@@ -33,10 +42,14 @@ void	s_error(char *error);
 void	s_error_fork(int sock);
 void	free_auth_struct(t_auth auth);
 void	free_2d_tab(void **tab);
-int		check_user(t_auth check);
-int		add_user(t_auth user);
+void	send_connexion_message(int msg, int sock);
 
-char	*auth(int sock);
+int		create_user(t_auth user, int sock, struct sockaddr_in sock_info);
+int		connect_user(t_auth check, int sock, struct sockaddr_in sock_info);
+
+t_auth			user_exist(char *user);
+
+void	auth(int sock, struct sockaddr_in sock_info);
 char	*crypto(char *str);
 
 #endif
